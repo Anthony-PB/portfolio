6c87d747febe8c5fd2af72db4cd5ce4f304cf2b7
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2024-09-19 01:56:33 -0400

    New Resume
    Still need to fix it a bit more.

--- src/components/s-components/CTA.js
+++ src/components/s-components/CTA.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./Small.css"
-import APB_PDF from "./DownloadFiles/Anthony_Paredes.pdf"
+import APB_PDF from "./DownloadFiles/Anthony_Paredes_Resume.pdf"
 
 function CTA() {
   return (
     <div className="spread">
       <h2>Greetings, I am Anthony Paredes</h2>
       <p>Computer Science Student | Tech Enthusiast</p>
-      <a className="downloadLink" href= {APB_PDF} download="Anthony_Paredes.pdf" rel="noopener noreferrer">
+      <a className="downloadLink" href= {APB_PDF} download="Anthony_Paredes_Resume.pdf" rel="noopener noreferrer">
       <h3 >Download My Resume</h3>
     </a>
     </div>
